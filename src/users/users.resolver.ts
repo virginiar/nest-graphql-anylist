9b7665f05089231fb05e7fada778d18ddfded366
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-04-29 12:32:42 +0200

    Protegiendo queries de los usuarios

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,26 +1,41 @@
+import { UseGuards } from '@nestjs/common';
 import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
+
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { ValidRolesArgs } from './dto/args/roles.arg';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { ValidRoles } from '../auth/enums/valid-roles.enum';
 
 @Resolver(() => User)
+@UseGuards(JwtAuthGuard)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
-  findAll(@Args() validRoles: ValidRolesArgs): Promise<User[]> {
+  findAll(
+    @Args() validRoles: ValidRolesArgs,
+    @CurrentUser([ValidRoles.admin, ValidRoles.superUser]) user: User,
+  ): Promise<User[]> {
     return this.usersService.findAll(validRoles.roles);
   }
 
   @Query(() => User, { name: 'user' })
-  findOne(@Args('id', { type: () => ID }) id: string): Promise<User> {
+  findOne(
+    @Args('id', { type: () => ID }) id: string,
+    @CurrentUser([ValidRoles.admin, ValidRoles.superUser]) user: User,
+  ): Promise<User> {
     // TODO:
     throw new Error('No implementado');
     // return this.usersService.findOne(id);
   }
 
   @Mutation(() => User)
-  blockUser(@Args('id', { type: () => ID }) id: string): Promise<User> {
+  blockUser(
+    @Args('id', { type: () => ID }) id: string,
+    @CurrentUser([ValidRoles.admin]) user: User,
+  ): Promise<User> {
     return this.usersService.block(id);
   }
 }
