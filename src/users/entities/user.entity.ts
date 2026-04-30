893499c3d1369c01cb4b5ba3e18902a27fa02406
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-04-30 13:26:07 +0200

    Creando la relación entre User e Item

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -4,8 +4,10 @@ import {
   Entity,
   JoinColumn,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { Item } from '../../items/entities/item.entity';
 
 @Entity({ name: 'users' })
 @ObjectType()
@@ -48,4 +50,8 @@ export class User {
   @JoinColumn({ name: 'lastUpdateBy' })
   @Field(() => User, { nullable: true })
   lastUpdateBy?: User;
+
+  @OneToMany(() => Item, (item) => item.user, { lazy: true })
+  @Field(() => [Item])
+  items!: Item[];
 }
